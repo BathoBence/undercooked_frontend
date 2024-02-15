4de67f37130809ec46b37a1d4f9fbd2d1ea7e635
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-02-15 15:52:28 +0100

    create recipe guide - contains rout, page, component

---
+++ undercooked/src/Components/Recipe/Recipe.jsx
@@ -0,0 +1,20 @@
+import './Recipe.css'
+
+const Recipe = ({ recipe }) => {
+  return (
+    <div className='recipe-guide'>
+      {recipe.name}
+      {recipe.ingredients.map(ingredient => {
+        return (
+          <li key={ingredient.material.name + ingredient.unit}>
+            {ingredient.amount} {ingredient.unit} {ingredient.material.name}
+          </li>
+        );
+      })}
+      <div>{recipe.guide}</div>
+    </div>
+  );
+};
+
+
+export default Recipe;
